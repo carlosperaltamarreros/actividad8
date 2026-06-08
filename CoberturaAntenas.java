524d747d594fc67df0559da0cdbeb3f91e32479e
orson98 <carlosperaltamarreros>
2026-06-08 02:50:53 -0500

    main_antenas

---
+++ CoberturaAntenas.java
@@ -0,0 +1,11 @@
+public class CoberturaAntenas {
+    public static void main(String[] args) {
+        System.out.println("--- EJEMPLO 1 ---");
+        int[] casas1 = {1, 2, 7, 11, 20, 21, 30};
+        
+
+        System.out.println("\n--- EJEMPLO 2 ---");
+        int[] casas2 = {2, 4, 8, 15, 18, 22};
+        
+    }
+}
