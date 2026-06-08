73815e599fef9b14ad9b15035f4feba775205337
orson98 <carlosperaltamarreros>
2026-06-08 02:15:00 -0500

    main1

---
+++ prom1/MochilaFraccional.java
@@ -0,0 +1,16 @@
+public class MochilaFraccional{
+
+    public static void main(String[] args) {
+        System.out.println("--- EJEMPLO 1 ---");
+        String[] nombres1 = {"A", "B", "C"};
+        double[] valores1 = {60, 100, 120};
+        double[] pesos1 = {10, 20, 30};
+    
+
+        System.out.println("\n--- EJEMPLO 2 ---");
+        String[] nombres2 = {"A", "B", "C"};
+        double[] valores2 = {80, 100, 120};
+        double[] pesos2 = {20, 10, 30};
+        
+    }
+}
